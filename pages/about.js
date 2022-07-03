5348be20f08b03abc38b761151e1188767b41c05
Oscar Ivan Anillo Garcia <oscar.anillo@globant.com>
2022-07-03 13:52:36 -0500

    PagesAndRoutes
    update the index.js root file
    create about route
    create ninjas route

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+export default function About() {
   return (
     <>
-      <h1>Home Component</h1>
+      <h1>About</h1>
       <p>
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
